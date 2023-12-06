34747db1aa917d79f26176e2e98a4e85e7174397
Adilson Silva <up202212352@fe.up.pt>
2023-12-06 10:48:17 +0000

    -  units tests for orMap

--- crdt/src/test/java/sdle/crdt/implementations/ORMapTest.java
+++ crdt/src/test/java/sdle/crdt/implementations/ORMapTest.java
@@ -1,40 +1,69 @@
 package sdle.crdt.implementations;
 
-import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
-import java.util.TreeSet;
+import java.util.Map;
 import java.util.UUID;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 class ORMapTest {
+
+    public static final String KEY_ARROZ = "arroz";
+    public static final String KEY_FEIJAO = "feijao";
 
     @Test
     void map() {
         ORMap orMap = new ORMap();
-        orMap.put("arroz", new CCounter().inc(1));
-        orMap.get("arroz").inc(1);
-        Assertions.assertEquals(2, orMap.get("arroz").read());
+        CCounter counter = new CCounter();
+        counter.inc(2);
+        orMap.put(KEY_ARROZ, counter);
+        Map<String, CCounter> map = orMap.map();
+        assertEquals(1, map.size());
+        assertTrue(map.containsKey(KEY_ARROZ));
+        assertEquals(2, map.get(KEY_ARROZ).read());
     }
 
     @Test
     void context() {
+        ORMap orMap = new ORMap("orMap1");
+        CCounter counter = new CCounter("arroz1");
+        counter.inc(2);
+        counter.dec(1);
+        orMap.put(KEY_ARROZ, counter);
+        assertEquals("orMap1: ([arroz : 1])", orMap.toString());
+        // TODO: realvaliar esse resultado ... o context deveria refletir  o put ???
+        assertEquals("Context\nCC ()\nDC ()", orMap.context().toString());
     }
 
     @Test
     void get() {
-        TreeSet set = new TreeSet<>();
-        set.add(2);
-        set.add(3);
-        set.add(4);
-        Assertions.assertEquals(3, set.higher(2));
+        ORMap orMap = new ORMap("orMap1");
+        CCounter counter = new CCounter("arroz1");
+        counter.inc(2);
+        orMap.put(KEY_ARROZ, counter);
+        assertEquals("orMap1: ([arroz : 2])", orMap.toString());
     }
 
     @Test
     void put() {
+        ORMap orMap = new ORMap("orMap1");
+        CCounter counter = new CCounter("arroz1");
+        counter.inc(2);
+        orMap.put(KEY_ARROZ, counter);
     }
 
     @Test
     void remove() {
+        ORMap orMap = new ORMap("orMap1");
+        CCounter counter = new CCounter("arroz1");
+        counter.inc(2);
+        orMap.put(KEY_ARROZ, counter);
+        assertEquals(1, orMap.map().size());
+        orMap.put(KEY_FEIJAO, new CCounter());
+        assertEquals(2, orMap.map().size());
+        orMap.remove(KEY_ARROZ);
+        assertEquals("orMap1: ([feijao : 0])", orMap.toString());
     }
 
     @Test
@@ -45,33 +74,30 @@ class ORMapTest {
     void join() {
 
         ORMap orMap1 = new ORMap(UUID.randomUUID().toString());
-        orMap1.put("arroz", new CCounter(UUID.randomUUID().toString()));
-        orMap1.get("arroz").inc(2);
+        orMap1.put(KEY_ARROZ, new CCounter(UUID.randomUUID().toString()));
+        orMap1.get(KEY_ARROZ).inc(2);
         CCounter feijaoCounter = new CCounter(UUID.randomUUID().toString());
         feijaoCounter.inc(1);
-        orMap1.put("feijao", feijaoCounter);
-        System.out.println(orMap1.map().get("feijao").context());
+        orMap1.put(KEY_FEIJAO, feijaoCounter);
+        feijaoCounter.inc(2);
+        feijaoCounter.dec(1);
         feijaoCounter.inc(1);
-        System.out.println(orMap1.map().get("feijao").context());
-        feijaoCounter.inc(1);
-        System.out.println(orMap1.map().get("feijao").context());
-        feijaoCounter.inc(1);
-        System.out.println(orMap1.map().get("feijao").context());
 
 
         ORMap orMap2 = new ORMap(UUID.randomUUID().toString());
-        orMap2.put("feijao", new CCounter(UUID.randomUUID().toString()));
-        orMap2.get("feijao").inc(2);
-        orMap2.get("feijao").inc(1);
-        orMap2.remove("feijao");
-        orMap2.put("feijao", new CCounter(UUID.randomUUID().toString()));
-        orMap2.get("feijao").inc(1);
+        orMap2.put(KEY_FEIJAO, new CCounter(UUID.randomUUID().toString()));
+        orMap2.get(KEY_FEIJAO).inc(2);
+        orMap2.get(KEY_FEIJAO).dec(1);
+        orMap2.remove(KEY_FEIJAO);
+        orMap2.put(KEY_FEIJAO, new CCounter(UUID.randomUUID().toString()));
+        orMap2.get(KEY_FEIJAO).inc(1);
 
         orMap1.join(orMap2);
-        System.out.println(orMap1.map().get("feijao").context());
-        Assertions.assertEquals(2, orMap1.map().size());
+        assertEquals(2, orMap1.map().size());
 
-        Assertions.assertEquals(5, orMap1.get("feijao").read());
-        Assertions.assertEquals(2, orMap1.get("arroz").read());
+        assertAll(
+                () -> assertEquals(4, orMap1.get(KEY_FEIJAO).read()),
+                () -> assertEquals(2, orMap1.get(KEY_ARROZ).read())
+        );
     }
 }
